b2246de19867b11fdf7647e41b6da5b027c0f34e
Jayden King <itsjaydenking@gmail.com>
2024-07-11 15:02:56 -0700

    Dark Mode Inclusion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,22 @@
 // JavaScript for menu toggle
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const navList = document.querySelector("nav ul");
-  
-    menuToggle.addEventListener("click", function () {
-      navList.classList.toggle("show");
-    });
+  const menuToggle = document.getElementById("menu-toggle");
+  const navList = document.querySelector("nav ul");
+
+  menuToggle.addEventListener("click", function () {
+    navList.classList.toggle("show");
   });
-  
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const body = document.body;
+  const header = document.querySelector("header");
+  const footer = document.querySelector("footer");
+
+  darkModeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    header.classList.toggle("dark-mode");
+    footer.classList.toggle("dark-mode");
+  });
+});
