84e38490ece9716f334fe8bbbac7718a8f68fe1b
Jayden King <itsjaydenking@gmail.com>
2024-07-11 14:47:43 -0700

    Updating Portfolio Website

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+// JavaScript for menu toggle
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navList = document.querySelector("nav ul");
+  
+    menuToggle.addEventListener("click", function () {
+      navList.classList.toggle("show");
+    });
+  });
+  
